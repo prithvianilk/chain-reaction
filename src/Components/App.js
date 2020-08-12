ba71fd3645f360c9731f27517a8eb8e185f44ffa
OhhWear <thepirateking3d2y@gmail.com>
2020-08-12 13:04:18 +0530

    changed some typos

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,11 +3,11 @@ import Land from "./Land";
 import { Route, HashRouter } from "react-router-dom";
 
 const App = () => {
-  return (
-    <HashRouter>
-      <Route path="/" exact component={Land} />
-    </HashRouter>
-  );
+    return (
+        <HashRouter>
+            <Route path="/" exact component={Land} />
+        </HashRouter>
+    );
 };
 
 export default App;
