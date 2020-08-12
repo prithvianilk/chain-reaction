128fc473118f387b8d8bf745a64b7c24f5d75920
Zotako <mkvarun2001@gmail.com>
2020-08-12 11:39:52 +0530

    commit1

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Land from "./Land";
+import Form from "./Form";
+import chat from "./chat";
 import { Route, HashRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <HashRouter>
-      <Route path="/" exact component={Land} />
+      <Route path="/" exact component={chat} />
     </HashRouter>
   );
 };
