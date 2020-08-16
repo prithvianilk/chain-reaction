380a6914bfba2be92ee2f58f52ef6ff2c2cc1ff8
OhhWear <thepirateking3d2y@gmail.com>
2020-08-16 13:18:51 +0530

    added imports

--- src/Components/Rooms.js
+++ src/Components/Rooms.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { List, ListItem, Heading, Divider } from '@chakra-ui/core';
+import { Stack, Badge, List, ListItem, Heading, Divider } from '@chakra-ui/core';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
