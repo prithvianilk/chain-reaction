348ec7138f718159955aa508374122d669f0ee89
OhhWear <thepirateking3d2y@gmail.com>
2020-08-16 17:53:46 +0530

    Fixed chat message

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import queryString from "query-string";
+import ChatMessage from './ChatMessage';
 import io from "socket.io-client";
-import { Alert, AlertIcon, Heading, Input, Box, Button } from "@chakra-ui/core";
+import { Heading, Divider, Input, Box, Button } from "@chakra-ui/core";
 import "../Styles/Chat.css";
-import "../Styles/Form.css";
-import "../Styles/Land.css";
 let socket;
 
 const Chat = ({ location }) => {
@@ -13,89 +12,53 @@ const Chat = ({ location }) => {
 	const [gender, setGender] = useState("");
 	const [message, setMessage] = useState("");
 	const [messages, setMessages] = useState([]);
-
+	
 	useEffect(() => {
 		const { room, gender, srn } = queryString.parse(location.search);
 		socket = io(ENDPOINT);
-
+		
 		setSRN(srn);
 		setGender(gender);
-
+		
 		socket.emit("join", { srn, gender, roomNumber: room }, (error) => {
 			console.log(error);
 		});
-
+		
 		return () => {
 			socket.emit("disconnect");
 			socket.off();
 		};
 	}, [ENDPOINT, location.search]);
-
+	
 	useEffect(() => {
 		socket.on("message", (message) => {
 			setMessages((messages) => [...messages, message]);
-		});
-	}, []);
-
+		})
+	}, [])
+	
 	const sendMessage = (message) => {
 		if (message) {
 			socket.emit("sendMessage", message, () => setMessage(""));
 		}
 	};
-
-	const renderListItem = (message, userGender, dex) => {
-		if (userGender === undefined) {
-			return (
-				<div>
-					<Alert key={dex} status="success" margin="5px 0">
-						<AlertIcon />
-						{message}
-					</Alert>
-				</div>
-			);
-		}
-
-		return (
-			<div>
-				<Box
-					key={dex}
-					color="white"
-					backgroundColor={userGender === "male" ? "#3182ce" : "#d53f8c"}
-					display="table"
-					margin="10px 0"
-					padding="5px 10px"
-					borderRadius="15px"
-					wordBreak="break-word"
-					style={{
-						float: `${userGender === gender ? "right" : "left"}`,
-						boxSizing: "border-box ",
-						clear: `${userGender === gender ? "right" : "left"}`,
-					}}
-				>
-					{message}
-				</Box>
-			</div>
-		);
-	};
-
+	
 	return (
 		<div className="form">
 			<Heading>Chat</Heading>
-			<div>
+			<Divider />
+			<Box>
 				<Box style={{ clear: "both" }}>
-					{messages.map(({ message, gender }, dex) =>
-						renderListItem(message, gender, dex)
-					)}
+					{messages.map((message, dex) => (<ChatMessage message = {message.message} gender = {gender} dex = {dex} userGender = {message.gender}/>))}
 				</Box>
 				<Input
 					value={message}
 					onChange={(e) => setMessage(e.target.value)}
 					onKeyPress={(e) => (e.key === "Enter" ? sendMessage(message) : null)}
 				/>
-			</div>
+			</Box>
 			<Button onClick={() => sendMessage(message)}>Send</Button>
 		</div>
-	);
-};
+	)
+}
 
 export default Chat;
---
+++ src/Components/ChatMessage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Alert, AlertIcon, Box } from '@chakra-ui/core'
+
+const ChatMessage = ({ message, userGender, dex, gender }) => {
+    if (userGender === undefined) {
+        return (
+            <div>
+                <Alert key={dex} status="success" margin="5px 0">
+                    <AlertIcon />
+                    {message}
+                </Alert>
+            </div>
+        );
+    };
+    return (
+        <Box
+            position="relative"
+            display="flex"
+            flexDirection="column"
+            alignItems={`flex-${userGender === gender ? 'end' : 'start'}`}
+            margin="10px 0"
+        >
+            <Box
+                wordBreak="break-word"
+                key={dex}
+                color="white"
+                marginX="10px"
+                backgroundColor={userGender === "male" ? "#3182ce" : "#d53f8c"}
+                padding="5px 10px"
+                borderRadius="15px"
+            >
+                {message}
+            </Box>
+        </Box>
+    )
+}
+
+export default ChatMessage;
