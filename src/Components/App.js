51a31eb108d124e54ffa3022d8b9a91f6cc4e0db
OhhWear <thepirateking3d2y@gmail.com>
2020-08-16 13:11:08 +0530

    refactor x5

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Land from "./Land";
-import Chat from "./Chat2";
+import Chat from "./Chat";
 import { Route, HashRouter } from "react-router-dom";
 import Rooms from "./Rooms";
 
